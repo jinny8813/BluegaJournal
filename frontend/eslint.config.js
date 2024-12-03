444fad2fa4d638f3f43a6322aa16ebb2281723d8
chingyi lan <jinny8813@gmail.com>
2024-12-03 15:32:24 +0800

    debug cicd

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,44 +3,49 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import testingLibrary from "eslint-plugin-testing-library";
+
+// 清理 globals 中的空格
+const cleanGlobals = (globals) => {
+  return Object.fromEntries(
+    Object.entries(globals).map(([key, value]) => [key.trim(), value])
+  );
+};
 
 export default [
   { ignores: ["dist"] },
   {
     files: ["**/*.{js,jsx}"],
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-      "jest/globals": true,
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:react-hooks/recommended",
-      "plugin:testing-library/react",
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
       parserOptions: {
-        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+      },
+      globals: {
+        ...cleanGlobals(globals.browser),
+        ...globals.es2021,
+        ...globals.node,
+        jest: true,
+        expect: true,
+        test: true,
+        describe: true,
+        beforeEach: true,
+        afterEach: true,
+        it: true,
+        vi: true,
       },
     },
-    settings: { react: { version: "detect" } },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    settings: {
+      react: {
+        version: "detect",
       },
-      ecmaVersion: 12,
-      sourceType: "module",
     },
     plugins: {
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "testing-library": testingLibrary,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -48,11 +53,15 @@ export default [
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
       "react/jsx-no-target-blank": "off",
-      'react/react-in-jsx-scope': 'off',
+      "react/react-in-jsx-scope": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+      "testing-library/await-async-queries": "error",
+      "testing-library/no-await-sync-queries": "error",
+      "testing-library/no-container": "error",
+      "testing-library/prefer-screen-queries": "error",
     },
   },
 ];
