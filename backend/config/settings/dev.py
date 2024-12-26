ba5ff3b319fe89e92dbc0780380057339db7b2ff
chingyi lan <jinny8813@gmail.com>
2024-12-26 13:38:32 +0800

    fix deploy-dev db name lost

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -1,13 +1,23 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['dev.bluegajournal.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'dev.bluegajournal.com',
+    'localhost',
+    '127.0.0.1',
+    '3.24.138.130',
+    'dev.bluegajournal.com:8080',  # 添加帶端口的域名
+]
 
 # CORS 設置
 CORS_ALLOWED_ORIGINS = [
-    "http://dev.bluegajournal.com",
-    "http://localhost:5173",
+    "http://dev.bluegajournal.com:8080",
+    "http://localhost:8080",
+    "http://3.24.138.130:8080",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True  # 開發環境可以允許所有來源
 
 # 靜態文件設置
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -16,10 +26,27 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# 數據庫設置
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'bluega_journal_dev'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
+
 # 安全設置
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
+CSRF_TRUSTED_ORIGINS = [
+    "http://dev.bluegajournal.com:8080",
+    "http://localhost:8080",
+    "http://3.24.138.130:8080",
+]
 
 # 日誌設置
 LOGGING = {
@@ -37,7 +64,11 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console', 'file'],
-            'level': 'INFO',
+            'level': 'DEBUG',  # 開發環境使用 DEBUG 級別
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
         },
     },
 }
