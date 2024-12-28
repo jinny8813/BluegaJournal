7c305ae424149926d6c8e67ca07f413e0c393fd4
chingyi lan <jinny8813@gmail.com>
2024-12-28 14:47:26 +0800

    deploy-prod fix backend nginx

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,10 +1,16 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['bluegajournal.com', 'www.bluegajournal.com']
+ALLOWED_HOSTS = ['bluegajournal.com', 'www.bluegajournal.com', 'localhost', 'backend']
+
+# 確保這些設置正確
+CSRF_TRUSTED_ORIGINS = ['https://bluegajournal.com', 'https://www.bluegajournal.com']
+CORS_ALLOWED_ORIGINS = ['https://bluegajournal.com', 'https://www.bluegajournal.com']
+CORS_ALLOW_CREDENTIALS = True
 
 # 安全設置
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
