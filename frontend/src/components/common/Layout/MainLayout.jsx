1d58b59d6aea3e457dadf56781932b35fba185bb
chingyi lan <jinny8813@gmail.com>
2025-02-14 16:55:05 +0800

    change navbar features

--- frontend/src/components/common/Layout/MainLayout.jsx
+++ frontend/src/components/common/Layout/MainLayout.jsx
@@ -18,18 +18,8 @@ const MainLayout = ({ children }) => {
           {/* Navigation Links */}
           <div className="hidden md:flex h-full text-sm">
             <Link
-              to="/blog"
-              className={`h-full flex items-center px-6 text-white ${
-                location.pathname === "/blog"
-                  ? "bg-blue-400"
-                  : "hover:bg-blue-500 transition-colors duration-200"
-              }`}
-            >
-              部落格
-            </Link>
-            <Link
               to="/planner"
-              className={`h-full flex items-center px-6 text-white ${
+              className={`h-full flex items-center px-4 text-white ${
                 location.pathname === "/planner"
                   ? "bg-blue-400"
                   : "hover:bg-blue-500 transition-colors duration-200"
@@ -38,14 +28,24 @@ const MainLayout = ({ children }) => {
               電子手帳
             </Link>
             <Link
-              to="/shop"
-              className={`h-full flex items-center px-6 text-white ${
-                location.pathname === "/shop"
+              to="/flashcard"
+              className={`h-full flex items-center px-4 text-white ${
+                location.pathname === "/flashcard"
                   ? "bg-blue-400"
                   : "hover:bg-blue-500 transition-colors duration-200"
               }`}
             >
-              商店
+              單字閃卡
+            </Link>
+            <Link
+              to="/about"
+              className={`h-full flex items-center px-4 text-white ${
+                location.pathname === "/about"
+                  ? "bg-blue-400"
+                  : "hover:bg-blue-500 transition-colors duration-200"
+              }`}
+            >
+              關於我
             </Link>
           </div>
 
