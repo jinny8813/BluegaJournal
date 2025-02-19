5d349102c78dc94bcde8c99d15757422459f49e3
chingyi lan <jinny8813@gmail.com>
2025-02-19 16:29:31 +0800

    fix: prod db connection

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -79,10 +79,10 @@ LOGGING = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'bluegajournal'),
+        'NAME': os.environ.get('POSTGRES_DB', 'bluega_journal_prod'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'bluega_db_prod'),  # 改為 'postgres'，不是 'localhost'
+        'HOST': os.environ.get('POSTGRES_HOST', 'bluega_db_prod'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
